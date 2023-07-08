74d5679b570b75af0e406d024c017694862b095f
JhenYiKuo <136140369+JhenYiKuo@users.noreply.github.com>
2023-07-08 08:17:07 +0000

    modify

--- lesson6_2.py
+++ lesson6_2.py
@@ -1,23 +1,32 @@
 #!usr/bin/python3.10.4
 import random
-min = 1
-max = 100
-count = 0
-target = random.randint(min, max)
-print('=====猜數字遊戲開始=====')
 
-while True:
-    keyin = int(input(f'猜數字遊戲範圍{min}~{max}:'))
-    count += 1
-    if (keyin == target):
-        print(f'猜對了，答案是:{target}')
-        print(f'您總共猜了{count}次')
+def playgame():
+    min = 1
+    max = 100
+    count = 0
+    target = random.randint(min, max)
+    print(target)
+    print('=====猜數字遊戲開始=====')
+
+    while True:
+        keyin = int(input(f'猜數字遊戲範圍{min}~{max}:'))
+        count += 1
+        if (keyin == target):
+            print(f'猜對了，答案是:{target}')
+            print(f'您總共猜了{count}次')
+            break
+        elif keyin > target:
+            print('再小一點')
+            max = keyin -1
+        elif keyin < target:
+            print('再大一點')
+            min = keyin +1
+        print(f'您已經猜了{count}次')
+
+while(True):
+    playgame()
+    play_again = (input('還要再來一把嗎？(y,n)'))
+    if not (play_again == 'y'):
         break
-    elif keyin > target:
-        print('再小一點')
-        max = keyin -1
-    elif keyin < target:
-        print('再大一點')
-        min = keyin +1
-    print(f'您已經猜了{count}次')
 print('遊戲結束')
