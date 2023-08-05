07a3c08d377e4cf65d0337d82c8814cb2a8b7ae4
JhenYiKuo <136140369+JhenYiKuo@users.noreply.github.com>
2023-08-05 04:00:41 +0000

    modify

--- lesson13_3.py
+++ lesson13_3.py
@@ -2,19 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-t = np.arange(0.0,1.0,0.05)
-st.write(t)
+
+#介面
+value = st.slider('三角函數',min_value=0,max_value=10)
+
+t = np.arange(0.0,value,0.05)
 y1 = np.sin(2 * np.pi * t)
 y2 = np.cos(2 * np.pi * t)
-st.write(y1)
-st.write(y2)
 
 #製圖
 figure1 = plt.figure(figsize=(8,4))
-axes = figure1.add_subplot()
-axes.plot(y1) #只寫1個代表y軸
-axes.plot(y2)
+axes1 = figure1.add_subplot()
+axes1.plot(y1) #只寫1個代表y軸
+axes1.plot(y2)
 #plt.show()
 st.write(figure1)
 
-#終端機執行 streanlit run lesson13_3.py
+#終端機執行 streamlit run lesson13_3.py
